298a722b010b41bf6c65066915e58402ebd8d3cb
Luka Klaric <luka.klaric.1988@gmail.com>
2020-07-05 13:10:34 +0000

    new view

--- composeexample/urls.py
+++ composeexample/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView # Unused
+from composeexample.views import Home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', TemplateView.as_view(template_name="home.html")),
+    path('home/', Home.as_view()),
 ]
